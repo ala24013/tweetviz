4ca9cd5afe440d8e46a85241345e6ee6f6e7a111
Alan <ala24013@gmail.com>
2022-08-26 21:22:57 -0400

    adding client and server

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import World from './world';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <World />
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/world.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Map, Marker } from "pigeon-maps"
+
+export default function World() {
+  return (
+    <Map height={window.innerHeight-4} defaultCenter={[47.879, 8.6997]} defaultZoom={11} onBoundsChanged={({center, zoom, bounds, initial}) => {console.log(bounds)}}>
+      <Marker width={20} anchor={[47.879, 8.6997]} />
+    </Map>
+  )
+}
