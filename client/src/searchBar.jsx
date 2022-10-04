445abef8c065bb42eaa30c7b749bb5fa673639a1
Alan <ala24013@gmail.com>
2022-10-04 11:59:50 -0400

    adding more packages and updating search bar

--- client/src/searchBar.jsx
+++ client/src/searchBar.jsx
@@ -1,5 +1,28 @@
 import React from "react"
+import { TextInput, Button, Box, ActionIcon } from "@mantine/core"
+import { IconSearch } from "@tabler/icons"
+
+DEFAULT_SEARCH = "Ian"
 
 export default function SearchBar(props) {
-    return (<></>)
+    const [searchValue, setSearchValue] = useState(DEFAULT_SEARCH)
+
+    return (
+        <Box>
+            <TextInput
+                value={searchValue}
+                onChange={(event) => setSearchValue(event.currentTarget.value)}
+                radius="xl"
+                size="lg"
+                error={/^[0-9a-zA-Z]{0,50}$/.test(searchValue) ? null : "ASCII characters and Numbers only!"}
+                rightSection={
+                    <Button variant="subtle">
+                        <ActionIcon variant="subtle">
+                            <IconSearch />
+                        </ActionIcon>
+                    </Button>
+                }
+            />
+        </Box>
+    )
 }
